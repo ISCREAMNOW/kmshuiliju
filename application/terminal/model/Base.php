da5605c805d6bf9dfb67947b958361491642f25f
liuzhiyong <1475633402@qq.com>
2020-09-18 10:15:00 +0800

    设置时间格式获取器

--- application/terminal/model/Base.php
+++ application/terminal/model/Base.php
@@ -8,5 +8,9 @@ use think\Model;
 
 class Base extends Model
 {
-
+   //数据库时间字段的自动转化
+    public function getAddTimeAttr($value){
+        $time=date('y-m-d H:m:s',$value);
+        return $time;
+    }
 }
